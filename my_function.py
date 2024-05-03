4732ba96ab5fd41223e179e86bcc67873480cd6e
sagar-metta <sagar.gaigawale@mettasocial.com>
2024-05-03 08:38:11 +0530

    fixed aws keys

--- my_function.py
+++ my_function.py
@@ -1,6 +1,11 @@
 import json
+import boto3
+
 
 def lambda_handler(event, context):
+    # dynamodb = boto3.resource("dynamodb")
+    # table_name = "StudentDetails"
+    # table = dynamodb.Table(table_name)
     # TODO implement
     return {
         'statusCode': 200,
